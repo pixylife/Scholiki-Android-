7df3e9a825a05fd1c05ac032b9258c60a6933b6a
Sahan Wijayananda <sahanw@ellipsis.lk>
2018-11-07 11:57:39 +0530

    Initialized login page

--- app/src/main/java/com/azio/scholiki/ui/MainActivity.kt
+++ app/src/main/java/com/azio/scholiki/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.azio.scholiki
+package com.azio.scholiki.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.azio.scholiki.R
 
 class MainActivity : AppCompatActivity() {
 
