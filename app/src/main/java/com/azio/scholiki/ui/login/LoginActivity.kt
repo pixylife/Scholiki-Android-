3ed6661b07dfb94a3b7d3e85d6721fcb9941311f
Sahan Wijayananda <sahanw@ellipsis.lk>
2018-11-08 09:05:58 +0530

    Login page basic UI initialized

--- app/src/main/java/com/azio/scholiki/ui/login/LoginActivity.kt
+++ app/src/main/java/com/azio/scholiki/ui/login/LoginActivity.kt
@@ -1,7 +1,8 @@
-package com.azio.scholiki.ui
+package com.azio.scholiki.ui.login
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.azio.scholiki.R
 
 class LoginActivity : AppCompatActivity() {
 
