52f9c96b34af2a5328a7a38e7e73bc085164483b
Sahan Thinusha <sahanw@ellipsis.lk>
2019-04-10 03:24:14 +0530

    Added reset password UI

---
+++ app/src/main/java/com/azio/scholiki/ui/reset_password/ResetPasswordActivity.kt
@@ -0,0 +1,13 @@
+package com.azio.scholiki.ui.reset_password
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.azio.scholiki.R
+
+class ResetPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
